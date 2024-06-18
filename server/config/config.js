923d82a25b50e479234e2809636b06809ea78007
Tomasz Pawliczak <tomasz.pawliczak@wundermanthompson.com>
2024-06-18 12:22:49 +0200

    database

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-const mongoURI = 'mongodb://localhost:27017/your_database_name';
+const mongoURI = 'mongodb+srv://pawliczaktomasz:mastakilla@rogalove.fmxj2oo.mongodb.net/?retryWrites=true&w=majority&appName=rogalove';
 const secretOrKey = 'your_secret_key';
 
 export { mongoURI, secretOrKey };
