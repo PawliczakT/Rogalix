f336b3663ff7dbfce735eb1461396dca01a8beb9
Tomasz Pawliczak <tomasz.pawliczak@wundermanthompson.com>
2024-06-27 14:58:01 +0200

    fix

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,4 @@ export const secretOrKey = process.env.SECRET_OR_KEY;
 export const emailUser = process.env.EMAIL_USER;
 export const emailPassword = process.env.EMAIL_PASSWORD;
 export const REACT_APP_API_URL=process.env.REACT_APP_API_URL;
+export const REACT_APP_API_KEY=process.env.REACT_APP_API_KEY;
