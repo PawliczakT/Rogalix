2573c426feddf562f9adef3e49b94c1d31ef3d0f
Tomasz Pawliczak <tomasz.pawliczak@wundermanthompson.com>
2024-07-18 11:48:43 +0200

    fix gustometr colors

--- client/src/components/Gustometr.js
+++ client/src/components/Gustometr.js
@@ -5,19 +5,38 @@ import { Container, Typography, Box, Table, TableBody, TableCell, TableHead, Tab
 const Gustometr = () => {
     const [matrix, setMatrix] = useState({});
     const [users, setUsers] = useState([]);
+    const [minValue, setMinValue] = useState(null);
+    const [maxValue, setMaxValue] = useState(null);
 
     useEffect(() => {
         const fetchMatrix = async () => {
             try {
                 const res = await api.get('/gustometr');
-                setMatrix(res.data);
-                setUsers(Object.keys(res.data));
+                const data = res.data;
+                setMatrix(data);
+                setUsers(Object.keys(data));
+
+                // Find min and max values in the matrix
+                let min = Infinity;
+                let max = -Infinity;
+
+                Object.values(data).forEach(row => {
+                    Object.values(row).forEach(value => {
+                        if (value !== null && value !== undefined) {
+                            if (value < min) min = value;
+                            if (value > max) max = value;
+                        }
+                    });
+                });
+
+                setMinValue(min);
+                setMaxValue(max);
             } catch (err) {
                 console.error(err);
             }
         };
 
-        fetchMatrix();
+        fetchMatrix().then(r => console.log('Fetched gustometr matrix'), e => console.error('Failed to fetch gustometr matrix:', e));
     }, []);
 
     return (
@@ -42,7 +61,22 @@ const Gustometr = () => {
                                 <TableCell>{user1}</TableCell>
                                 {users.map(user2 => (
                                     <TableCell key={user2}>
-                                        {matrix[user1][user2] !== undefined ? matrix[user1][user2] : '-'}
+                                        {matrix[user1][user2] !== undefined ? (
+                                            <span
+                                                style={{
+                                                    color:
+                                                        matrix[user1][user2] === maxValue
+                                                            ? 'green'
+                                                            : matrix[user1][user2] === minValue
+                                                                ? 'red'
+                                                                : 'inherit',
+                                                }}
+                                            >
+                                                {matrix[user1][user2]}
+                                            </span>
+                                        ) : (
+                                            '-'
+                                        )}
                                     </TableCell>
                                 ))}
                             </TableRow>
