ad71cb6d33c25cb82cedb59a7903ac808d1a43ac
Tomasz Pawliczak <tomasz.pawliczak@wundermanthompson.com>
2024-07-30 13:32:30 +0200

    fix

--- server/app.js
+++ server/app.js
@@ -1,44 +1,58 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import passport from 'passport';
 import path from 'path';
 import {fileURLToPath} from 'url';
-import passport from 'passport';
-import configurePassport from './config/passport.js';
+import {mongoURI as db} from './config/config.js';
 import users from './routes/auth.js';
 import rogals from './routes/rogals.js';
 import gustometr from './routes/gustometr.js';
+import passportConfig from './config/passport.js';
+import simpleGit from 'simple-git';
 
+dotenv.config();
+const git = simpleGit();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
-
-// Body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-const db = process.env.MONGO_URI;
 mongoose.connect(db)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-// Passport middleware
 app.use(passport.initialize());
 
-// Passport Config
-configurePassport(passport);
+passportConfig(passport);
 
-// Use Routes
 app.use('/api/users', users);
 app.use('/api/rogals', rogals);
 app.use('/api/gustometr', gustometr);
 
-// Serve static assets if in production
+app.get('/api/git-info', async (req, res) => {
+    try {
+        const branchSummary = await git.branchLocal();
+        const currentBranch = branchSummary.current;
+        const commitSummary = await git.log({n: 1});
+        const latestCommitHash = commitSummary.latest.hash;
+
+        res.json({
+            branch: currentBranch,
+            commit: latestCommitHash,
+        });
+    } catch (err) {
+        console.error('Failed to get Git info', err);
+        res.status(500).json({error: 'Failed to get Git info'});
+    }
+});
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -47,11 +61,9 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-// Serve static files from the uploads directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 export default app;
