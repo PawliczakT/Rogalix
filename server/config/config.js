526a30cb357023f99bcb3d7b9b95c2ed93ec5314
Tomasz Pawliczak <tomasz.pawliczak@wundermanthompson.com>
2024-07-31 17:51:29 +0200

    fix

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const mongoURI = process.env.MONGO_URI;
-export const secretOrKey = process.env.SECRET_OR_KEY;
+export const secretOrKey = process.env.JWT_SECRET;
 export const emailUser = process.env.EMAIL_USER;
 export const emailPassword = process.env.EMAIL_PASSWORD;
 export const REACT_APP_API_URL=process.env.REACT_APP_API_URL;
